7d0690f98a7919b612657deafd581d114e043fff
asman255 <smandth@gmail.com>
2025-01-31 21:27:05 +0700

    backend controller model for user

---
+++ backend/controllers/userController.js
@@ -0,0 +1,57 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+// Route for user login
+const loginUser = async (req, res) => {};
+
+// Route for user register
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // check user already exist
+    const exist = await userModel.findOne({ email });
+    if (exist) {
+      return res.json({ success: false, message: "User already exist" });
+    }
+
+    // validating email format and strong password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {}
+};
+
+// Route for admin login
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
