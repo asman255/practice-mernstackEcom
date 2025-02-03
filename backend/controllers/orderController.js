a85dac1ea464b48f8d2d863a89867d25c37e292c
asman255 <smandth@gmail.com>
2025-02-03 23:54:50 +0700

    place order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,8 +1,27 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
 // place orders using COD method
 const placeOrder = async (req, res) => {
   try {
     const { userId, items, amount, address } = req.body;
-  } catch (error) {}
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+    res.json({ success: true, messege: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, messege: error.messege });
+  }
 };
 // place orders using stripe method
 const placerderStripe = async (req, res) => {};
